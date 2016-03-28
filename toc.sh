0f47b68affb92890d791548d4811493984242936
Jason Peak <jpeak5@lsu.edu>
2016-03-28 00:09:56 -0400

    dont overwrite index.html

--- toc.sh
+++ toc.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-java -cp saxon/saxon9he.jar net.sf.saxon.Transform  -s:toc.xsl -xsl:toc.xsl -o:index.html
+java -cp saxon/saxon9he.jar net.sf.saxon.Transform  -s:toc.xsl -xsl:toc.xsl -o:toc.html
